bdd5134004a06b805c4bd9177d83c5e193eeaf81
Yash Joshi <yashjoshi3995@gmail.com>
2023-08-19 14:31:38 +0100

    minor:

--- C/structPadding.c
+++ C/structPadding.c
@@ -12,26 +12,29 @@ typedef struct sample
 
 int main()
 {
-   /* Variable declaration of the sample type..
+   /* Variable declaration of a bad sample type..
     */
-   SAMPLE_T test;
+   SAMPLE_T test_bad;
 
    printf("\n");
 
    /* Displaying the size of the structure sample.
     */
-   printf("The size of the sample structure is sizeof(test):(%ld).\n",
-          sizeof(test));
+   printf("The size of the sample structure is sizeof(test_bad):(%ld).\n",
+          sizeof(test_bad));
 
    /* Displaying the address of each structure member.
     */
-   printf("The address of test.a:(%p)\n", &test.a);
-   printf("The address of test.b:(%p)\n", &test.b);
-   printf("The address of test.c:(%p)\n", &test.c);
+   printf("The address of test_bad.a:(%p)\n", &test_bad.a);
+   printf("The address of test_bad.b:(%p)\n", &test_bad.b);
+   printf("The address of test_bad.c:(%p)\n", &test_bad.c);
 
    printf("\n");
    printf("So, you can observe that,\nThis(%p) and This(%p) are unused/unusable memory spaces.\n",
-          &test.b + 1, &test.b + 2);
+          &test_bad.b + 1, &test_bad.b + 2);
+
+   printf("\n");
+   printf("\n");
 
    return 0;
 }
